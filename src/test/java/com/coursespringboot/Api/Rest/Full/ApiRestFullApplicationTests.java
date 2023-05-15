482d0949cbb442e68091ef48e2aa6f90f6f2e198
KaykyB <kaykyb629@gmail.com>
2023-05-15 11:19:45 -0300

    First Commit

---
+++ src/test/java/com/coursespringboot/Api/Rest/Full/ApiRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursespringboot.Api.Rest.Full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
