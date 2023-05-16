3844d8c8b55efe86eeb17af9d180db848f0679e1
KaykyB <kaykyb629@gmail.com>
2023-05-16 09:03:48 -0300

    Add PutMapping this Project.

--- src/main/java/com/coursespringboot/Api/Rest/Full/controllers/StudentController.java
+++ src/main/java/com/coursespringboot/Api/Rest/Full/controllers/StudentController.java
@@ -32,7 +32,7 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<Object> saveStudent(@RequestBody @Valid StudentDto studentDto){
         if (studentService.existsByEmail(studentDto.getEmail())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already registered.");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(" Email already registered.");
         }
 
         var studentModel = new StudentModel();
@@ -45,14 +45,28 @@ public class StudentController {
     public ResponseEntity<Object> deleteStudent(@PathVariable(value = "id") Long id){
         Optional<StudentModel> existsStudent = studentService.findById(id);
         if(!existsStudent.isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(" Student not found.");
         }
 
         studentService.delete(existsStudent.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Studend deleted successfully.");
+        return ResponseEntity.status(HttpStatus.OK).body(" Student deleted successfully.");
     }
 
-    public ResponseEntity post(){
-        return null;
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateStudent(@PathVariable(value="id") Long id,
+                                                @RequestBody @Valid StudentDto studentDto){
+        Optional<StudentModel> studentModelOptical = studentService.findById(id);
+        if(!studentModelOptical.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(" Student not found.");
+        }
+        if (studentService.existsByEmail(studentDto.getEmail())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(" Email already registered.");
+        }
+
+        var studentModel = new StudentModel();
+        BeanUtils.copyProperties(studentDto, studentModel);
+        studentModel.setId(studentModelOptical.get().getId());
+        studentModel.setAge(Period.between(studentModelOptical.get().getDob(), LocalDate.now()).getYears());
+        return ResponseEntity.status(HttpStatus.OK).body(studentService.save(studentModel));
     }
 }
