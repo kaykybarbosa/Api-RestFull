1809ef81a24e271a0f39d073a24258d58a0e2425
KaykyB <kaykyb629@gmail.com>
2023-05-16 10:09:10 -0300

    Settings Files

--- src/main/java/com/coursespringboot/Api/Rest/Full/controllers/StudentController.java
+++ src/main/java/com/coursespringboot/Api/Rest/Full/controllers/StudentController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
